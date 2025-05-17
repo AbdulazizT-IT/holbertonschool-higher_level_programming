e67c5b818991a587f27edf0de50b3e652c1af36e
Abdulaziz <94856585+AbdulazizT-IT@users.noreply.github.com>
2025-05-17 19:44:33 +0300

    0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,6 +3,7 @@
 This module provides a function to add two integers.
 """
 
+
 def add_integer(a, b=98):
     """Adds two integers or floats after casting them to integers."""
     if not isinstance(a, (int, float)):
