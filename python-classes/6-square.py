ec9dcd4d1c6ab14776d2ae6f078bc46b26696c75
AbdulazizT-IT <almuteare305@gmail.com>
2025-05-20 16:04:45 +0000

    6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,82 +1,57 @@
 #!/usr/bin/python3
-"""Defines an class Square."""
+"""Defines a class Square with size and position."""
 
 
 class Square:
-    """Represents a square with size and position."""
+    """Represents a square."""
 
     def __init__(self, size=0, position=(0, 0)):
-        """Creates a square of a given size
-
-        Size of the square is hidden
-
-        Args:
-            size (int): length of the sides
-            position (tuple): the position of the square
-
-        Raises:
-            TypeError: size is not an integer
-            ValueError: size is negative
-
-        """
-
         self.size = size
         self.position = position
 
     @property
     def size(self):
+        """Getter for size."""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """Setter for size with validation."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-    def area(self):
-        """Returns the size of square
-
-        Returns:
-            size squared for area
-
-        """
-
-        return self.__size ** 2
-
     @property
     def position(self):
+        """Getter for position."""
         return self.__position
 
     @position.setter
     def position(self, value):
-        if(
-            not isinstance(value, tuple)
-            or len(value) != 2
-            or not all(isinstance(num, int) for num in value)
-            or not all(num >= 0 for num in value)
-            ):
+        """Setter for position with validation."""
+        if (not isinstance(value, tuple) or
+            len(value) != 2 or
+            not all(isinstance(num, int) for num in value) or
+            not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
+    def area(self):
+        """Returns area of the square."""
+        return self.__size ** 2
+
     def my_print(self):
-        """Prints out a grid of #'s representing the sqaure
-
-        prints a blank line if size is 0
-
-        also moves the sqaure to match position
-
-        """
-
+        """Prints the square with '#' considering position."""
         if self.__size == 0:
             print()
+            return
 
-        for y in range(self.__position[1]):
-            print("")
-        for i in range(self.__size):
-            for x in range(self.__position[0]):
-                print(' ', end='')
-            for j in range(self.__size):
-                print('#', end='')
+        # Vertical position: print empty lines
+        for _ in range(self.__position[1]):
             print()
+
+        # Print each line with horizontal spaces then hashes
+        for _ in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
