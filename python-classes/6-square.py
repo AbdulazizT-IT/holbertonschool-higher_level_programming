88cd328a89633814baa2cac42aec750223c25290
Abdulaziz <94856585+AbdulazizT-IT@users.noreply.github.com>
2025-05-21 17:44:39 +0300

    Update 6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -70,6 +70,5 @@ class Square:
         for _ in range(self.__position[1]):
             print()
 
-        # Print each line of the square with spaces before it (horizontal space)
         for _ in range(self.__size):
             print(" " * self.__position[0] + "#" * self.__size)
