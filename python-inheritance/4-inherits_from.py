d0a1763a0ddc830f2b1d4c4609d83fdbfe8eaf72
AbdulazizT-IT <almuteare305@gmail.com>
2025-05-28 15:41:44 +0000

    4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -10,7 +10,7 @@ def inherits_from(obj, a_class):
     """
     Returns True or False.
     """
-    if isinstance(obj, a_class) and type(obj) != a_class:
+    if isinstance(obj, a_class) and type(obj) is not a_class:
         return True
     else:
         return False
