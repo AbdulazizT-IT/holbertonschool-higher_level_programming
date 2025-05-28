ac900f3243ff354ea6216730921c7b8797a44f21
AbdulazizT-IT <almuteare305@gmail.com>
2025-05-28 15:21:57 +0000

    4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module defines a function that returns Ture if the object is an instance
+of a class that inherited (directly or indirectly) from the specified class;
+otherwise False.
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Returns True or False.
+    """
+    if type(obj) is not a_class:
+        return True
+    else:
+        return False
