5d6f1793753046cd19d67febcf9541d85d988e53
AbdulazizT-IT <almuteare305@gmail.com>
2025-06-01 17:03:56 +0000

    2

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -9,11 +9,12 @@ class VerboseList(list):
 
     def extend(self, iterable):
         count = len(iterable)
-        super().extend(iterable)
         print(f"Extended the list with [{count}] items.")
+        super().extend(iterable)
 
     def remove(self, item):
         print(f"Removed [{item}] from the list.")
+        super().remove(item)
 
     def pop(self, index=-1):
         item = self[index]
