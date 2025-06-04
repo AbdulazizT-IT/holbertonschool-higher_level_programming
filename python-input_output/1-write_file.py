90700ce2b9adb9a2a6244cf49344681499f63514
AbdulazizT-IT <almuteare305@gmail.com>
2025-06-04 15:43:28 +0000

    1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,4 +6,4 @@ def write_file(filename="", text=""):
     """Function for write_file"""
 
     with open(filename, 'w', encoding='UTF-8') as f:
-        return(f.write(text))
+        return (f.write(text))
