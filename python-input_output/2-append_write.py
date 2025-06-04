e1d95cebfed083c7007f134a385adad22f03757d
AbdulazizT-IT <almuteare305@gmail.com>
2025-06-04 15:50:35 +0000

    2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """Module for append_file"""
 
 
-def append_file(filename="", text=""):
+def append_write(filename="", text=""):
     """Function for append_file"""
 
     with open(filename, 'a', encoding='UTF-8') as f:
