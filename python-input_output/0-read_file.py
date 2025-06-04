64f8a4186e9a359a70aa247ce75daab7cd48a875
AbdulazizT-IT <almuteare305@gmail.com>
2025-06-04 15:26:45 +0000

    0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for read_file"""
+
+
+def read_file(filename=""):
+    """Function for read_file"""
+    
+    with open(filename, 'r', encoding='UTF-8') as f:
+        files = f.read()
