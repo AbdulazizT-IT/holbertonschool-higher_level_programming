a5d7c9434a7afe0e49fea0fb312bfd8b32a00870
AbdulazizT-IT <almuteare305@gmail.com>
2025-06-04 15:36:15 +0000

    1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for write_file"""
+
+
+def write_file(filename="", text=""):
+    """Function for write_file"""
+
+    with open(filename, 'r', encoding='UTF-8') as f:
+        print(f.write(text), end="")
+    return len(text)
