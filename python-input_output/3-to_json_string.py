21393e2c965fc6df8238e9c46e3cd3bd460c1c9b
AbdulazizT-IT <almuteare305@gmail.com>
2025-06-16 13:36:51 +0000

    3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import json
+"""Module for JASON to string"""
+
+
+def to_json_string(my_obj):
+    """Function for JASON TO string"""
+
+    y = json.loads(my_obj)
+    return y
