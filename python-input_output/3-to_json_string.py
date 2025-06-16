d6aa8cfa784fb645da8ba6e9e7d03ce642afb02b
AbdulazizT-IT <almuteare305@gmail.com>
2025-06-16 13:42:46 +0000

    3

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 import json
-"""Module for JASON to string"""
-
+"""Module for converting Python objects to JSON string representation."""
 
 def to_json_string(my_obj):
-    """Function for JASON TO string"""
+    """
+    Returns the JSON string representation of a Python object.
 
-    y = json.loads(my_obj)
-    return y
+    Args:
+        my_obj: The Python object to convert (e.g. dict, list, etc.)
+
+    Returns:
+        str: The JSON string representation of the object.
+    """
+    return json.dumps(my_obj)
