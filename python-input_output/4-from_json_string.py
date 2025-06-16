a9221d947986ecd424810c5ab9be2e65b576074c
AbdulazizT-IT <almureare305@gmail.com>
2025-06-16 14:46:57 +0000

    4

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+"""Module for converting Python objects to JSON string representation."""
+
+
+import json
+
+
+def to_json_string(my_obj):
+
+    """
+    Returns the JSON string representation of a Python object.
+
+    Args:
+        my_obj: The Python object to convert (e.g. dict, list, etc.)
+
+    Returns:
+        str: The JSON string representation of the object.
+    """
+
+    return json.loads(my_obj)
