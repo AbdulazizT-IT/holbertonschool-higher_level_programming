a3ce287cea460ab48ea75b3bf60dde5f4e725cb8
AbdulazizT-IT <almuteare305@gmail.com>
2025-06-26 15:03:26 +0000

    9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,7 +1,9 @@
 -- creates a table with multiple rows
 
-CREATE TABLE IF NOT EXIST second_table(
+CREATE TABLE IF NOT EXIST second_table
+(
     id INT,
-    name VARCHAR(256)
+    name VARCHAR(256),
     score INT
-) WITH VALUES(1, 'John', 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
+)
+VALUES(1, 'John', 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
