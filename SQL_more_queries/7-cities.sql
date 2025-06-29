e6beeeb10428a7ef5b210019a3ed1b049f461d73
Your Name <you@example.com>
2025-06-29 17:34:16 +0000

    Cities table

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -5,4 +5,4 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     state_id INT NOT NULL FOREIGN KEY REFERENCES states(id),
     name VARCHAR(256)
-)
+);
