1b146a6feade3d0c8e133ac40e71c280eeccf0b8
Your Name <you@example.com>
2025-06-30 18:02:18 +0000

    edit file 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -19,7 +19,11 @@ if __name__ == "__main__":
     """
 
     db = MySQLdb.connect(
-        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], host="localhost", port=3306
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        host="localhost",
+        port=3306,
     )
 
     cursor = db.cursor()
