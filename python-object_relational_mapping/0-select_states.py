e3fcd8f4c38befa60acbb8e4e7739db63b4cc959
Your Name <you@example.com>
2025-06-30 17:11:14 +0000

    get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
 """
 Lists all states from the database hbtn_0e_0_usa
+
+This script connects to a MySQL database, retrieves all the states
+from the `states` table, and displays them in ascending order by `id`.
 """
 
 
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+if __name__ == "__main__":
+    """
+    This script takes 3 arguments (mysql username, mysql password,
+    and database name) and lists all states from the `states` table
+    in the given database, ordered by the `id` in ascending order.
+    """
 
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                         db=sys.argv[3], port=3306)
@@ -21,6 +26,6 @@ if __name__ == "__main__":
     query = "SELECT * FROM states ORDER BY id ASC"
     cursor.execute(query)
     states = cursor.fetchall()
-    
+
     for state in states:
         print(state)
