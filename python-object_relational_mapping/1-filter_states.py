0fc30d96295162e22705a3525aa013b12edfe465
Your Name <you@example.com>
2025-06-30 17:29:45 +0000

    filter states

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Lists all states from the database hbtn_0e_0_usa
+
+This script connects to a MySQL database, retrieves all the states
+from the `states` table, and displays them in ascending order by `id`.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """
+    This script takes 3 arguments (mysql username, mysql password,
+    and database name) and lists all states from the `states` table
+    in the given database, ordered by the `id` in ascending order.
+    """
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+
+    query = "SELECT * FROM states WHERE NAME LIKE 'H%' ORDER BY states.id ASC"
+    cur.execute(query)
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
