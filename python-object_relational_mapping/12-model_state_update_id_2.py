f45369f2e5333386cf48e040f3d735bfb2c42ce6
Your Name <you@example.com>
2025-07-01 13:05:42 +0000

    all

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-
 """
-Lists all State objects that contain the letter a from the database
+Changes the name of a State object from the database hbtn_0e_6_usa
 """
-
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+
 if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states_with_a = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+    stateUpdated = session.query(State).filter(State.id == 2).first()
 
-    for state in states_with_a:
-        print("{}: {}".format(state.id, state.name))
+    if stateUpdated:
+        stateUpdated.name = 'New Mexico'
+        session.commit()
--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-
 """
-Lists all State objects that contain the letter a from the database
+Deletes all State objects with a name containing the letter a
 """
-
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+
 if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states_with_a = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+    states = session.query(State).filter(State.name.like('%a%')).all()
 
-    for state in states_with_a:
-        print("{}: {}".format(state.id, state.name))
+    for state in states:
+        session.delete(state)
+
+    session.commit()
