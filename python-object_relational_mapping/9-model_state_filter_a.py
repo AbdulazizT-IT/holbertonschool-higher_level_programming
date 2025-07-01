e4989df781798bfcce8149a31d40f4751812404f
Your Name <you@example.com>
2025-07-01 12:58:44 +0000

    Contains 'a'

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
 Session = sessionmaker(bind=engine)
 session = Session()
 
-states_with_a = session.query(State).filter(State.name.like('%a%')).all()
+states_with_a = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
 
 for state in states_with_a:
     print("{}: {}".format(state.id, state.name))
