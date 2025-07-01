45efa875b4d574df942062960e3d1f63e3112613
Your Name <you@example.com>
2025-07-01 10:28:02 +0000

    First state model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -15,7 +15,17 @@ Base = declarative_base()
 
 
 class State(Base):
-    __tablename__ = 'states'
+    """
+    Represents the 'states' table in the database.
+
+    Attributes:
+    id (Integer): Primary key column,
+    unique identifier for each state, not nullable.
+    name (String): Name of the state,
+    max length 128 characters, not nullable.
+    """
+
+    __tablename__ = "states"
 
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
