e1e9dfd5de2e1d8a05b405998e20ada0bf6f3a6c
Your Name <you@example.com>
2025-07-01 07:30:33 +0000

    filter states by user input

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+"""
+Lists all rows in 'states' table where name
+matches the argument.
+Usage: ./2-my_filter_states.py <username>
+<password> <db_name> <state_name>
+"""
 
 import sys
 import MySQLdb
+
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
