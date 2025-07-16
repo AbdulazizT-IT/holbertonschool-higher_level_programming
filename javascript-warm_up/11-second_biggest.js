76615b2ca6c99e414b7d8f92c73a94db7ee4d6aa
Your Name <you@example.com>
2025-07-16 14:34:41 +0000

    11

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2).map(x => parseInt(x));
+
+if (args.length < 2) {
+  console.log(0);
+} else {
+  const uniqueSorted = [...new Set(args)].sort((a, b) => b - a);
+  console.log(uniqueSorted.length < 2 ? 0 : uniqueSorted[1]);
+}
