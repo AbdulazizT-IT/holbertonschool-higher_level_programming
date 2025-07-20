b443120ff86ee9ab2802eac711b8a50af97b93d5
Your Name <you@example.com>
2025-07-20 16:11:40 +0000

    task 3

---
+++ python-server_side_rendering/ task_02_logic.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+import json
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/items')
+def items():
+    try:
+        with open('items.json') as f:
+            data = json.load(f)
+            items = data.get('items', [])
+    except Exception:
+        items = []
+
+    return render_template('items.html', items=items)
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
